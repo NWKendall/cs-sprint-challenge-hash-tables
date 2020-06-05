09235f257e9eb6e738417cf21c9dc3d695f6794c
nicbongo <nicbongo@protonmail.com>
2020-06-05 12:18:54 -0300

    ex1 passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,31 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    list_of_weights = {}
+    answer = []
+    # compliment_index = None
+    for w in range(length):
+        result = limit - weights[w]
+        if result in list_of_weights.keys():
+            second_index = weights.index(result)
+            if w != second_index:
+                return [w, second_index]
 
-    return None
+        list_of_weights.update({weights[w]: w})
+    
+
+# https://levelup.gitconnected.com/two-sum-interview-problem-in-python-c1d84b029d35
+l = [ 4, 6, 10, 15, 16 ]
+
+print(get_indices_of_item_weights(l, 5, 21))
+# weights list of ints
+# lenth is number of weights in length
+# limit is target
+# use a dict to store values
+    # array of values
+
+"""
+Example:
+input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+"""
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,14 +1,48 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+import random
+
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
+def reconstruct_trip(tickets):
     """
     YOUR CODE HERE
     """
     # Your code here
+    flights = {}
+    journey = []
+    for t in tickets:
+        # populate dictionary
+        print("TICKET SOURCE:", t.source)
+        if flights[t.source] == t.source:
+            journey.append(t.source)
+        else:
+            flights[t.source] = t.destination
+        
+    return journey
 
-    return route
+
+trip = [Ticket("PIT", "ORD"),
+        Ticket("XNA", "SAP"),
+        Ticket("SFO", "BHM"),
+        Ticket("FLG", "XNA"),
+        Ticket("NONE", "LAX"),
+        Ticket("LAX", "SFO"),
+        Ticket("SAP", "SLC"),
+        Ticket("ORD", "NONE"),
+        Ticket("SLC", "PIT"),
+        Ticket("BHM", "FLG")]
+
+
+print(reconstruct_trip(trip))
+# markov table ✅
+# need dict to place tickets ✅
+# populate dictionary with tickets ✅
+# populate journey
+# return array of locations (journey)
+    # starts with None
+    # Ends with None
+    # Remove None from return array
